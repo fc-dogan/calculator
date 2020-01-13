1320892f575cb4e2a24fc90536d8daf12f825fd4
fatma dogan and jeremy kale padot <jeremy@>
2020-01-13 11:30:53 -0800

    add BMI calculator, commented out for practice

--- js/script.js
+++ js/script.js
@@ -18,3 +18,14 @@ var add = function(number1, number2) {
     var number2 =parseInt(prompt("Enter another number: "));
     var result =divide(number1, number2);
     alert(result);
+
+// this is the code for a BMI calculator from the exercise
+//     var height=parseInt(prompt("How inches tall are you?"));
+// var weight=parseInt(prompt("How much do you weigh?"));
+
+// var BMI = function(height, weight){
+// return (weight * 703)/(height * height)
+// };
+
+// var result = BMI (height, weight);
+// alert(result);
